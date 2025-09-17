f27af7ffc4e34afb3d867ed4c32dca2feb6f5d82
NellowTCS <neeljaiswal23@gmail.com>
2025-09-17 05:11:19 +0000

    Fix build

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const prod = process.env.NODE_ENV === 'production'
+const isExport = process.env.NEXT_EXPORT === 'true' 
 
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
+  output: isExport ? "export" : undefined, // only set output when exporting
   pwa: {
     dest: "public",
-    disable: !prod,
+    disable: !prod || isExport, // disable PWA for static export
     register: true,
     skipWaiting: true,
     runtimeCaching,
